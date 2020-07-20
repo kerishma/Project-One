f1a71605b0db341b699dada139e820ec141f0e36
Daniel Altman <63561695+Dangleboone@users.noreply.github.com>
2020-07-19 19:16:27 -0700

    Revert "deleted duplicated code, it was doing the same process, so reusing th…"

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ $("#randomBtn").click(function (event) {
   // this code is for the 'surprise me!' button
     event.preventDefault();
 
+    surpriseMe(userLocation);
 
 });
 
@@ -95,6 +96,43 @@ function createRestaurantCards(data) {
         cardDeck.append(card)
         $("#rCards").append(cardDeck);
     }
+
+}
+
+// loop for 'surprise me' button
+function randomRestaurantCards(data) {
+    let places = data.businesses;
+
+
+    for (let i = 0; i < 4; i++) {
+        let defaultUnknown = "Not Confirmed";
+        let cardDeck = $("<div class='card-deck'></div>");
+        let card = $("<div class='card mb-3'></div>");
+        let cardRow = $("<div class='row no-gutters'></div>");
+        let cardImgCol = $("<div class='col'></div>");
+        let yelpUrl = places[i].url
+        let yelpImg = $(`<a href=${yelpUrl} target="_blank"> <img class='card-img img-fluid' src=${places[i].image_url}></a>`);
+        let cardColBody = $("<div class='col-md-8'></div>");
+        let cardBody = $("<div class='card-body'>");
+        let cardH = $("<h5 class='card-title'></h5>").text(places[i].name);
+        let rating = $("<p class='card-text'></p>").text(`Rating: ${places[i].rating}`);
+        if (places[i].price) {
+          defaultUnknown = places[i].price  
+        }
+        let price = $("<p class='card-text'></p>").text(`Price: ${defaultUnknown}`)
+        let takeout = $("<p class='card-text'></p>").text(`Takeout: ${places[i].transactions}`);
+        if (places[i].phone) {
+            defaultUnknown = places[i].phone 
+        }
+        let phoneNum = $("<p class='card-text'></p>").text(`Phone: ${places[i].phone}`);
+        card.css("max-width", "540px");
+        cardImgCol.append(yelpImg);
+        cardBody.append(cardH, rating, price, takeout, phoneNum);
+        cardColBody.append(cardBody);
+        cardRow.append(cardImgCol, cardColBody)
+        card.append(cardRow);
+        cardDeck.append(card)
+        $("#rCards").append(cardDeck);
     }
 
 }
@@ -139,3 +177,31 @@ function getYelpData(data) {
     })
 }
 
+// adding function for 'surprise me button'
+function surpriseMe(data) {
+
+    let lat = data.latitude
+    let lon = data.longitude
+    let term = $("#search-bar").val()
+    // Build the query URL for the ajax request to the NYT API
+    const queryURL = buildQueryURL(term, lat, lon);
+
+    $.ajax({
+        url: queryURL + "&categories=restaurants", 
+        headers: {
+            'Content-Type': 'application/json',
+            "x-requested-with": "xmlhttprequest",
+            "Access-Control-Allow-Origin": "*",
+            "Authorization": `Bearer ${config.apiKey}`,
+        },
+        crossDomain: true,
+        method: "GET",
+        success: function (result) {
+            randomRestaurantCards(result);
+            console.log(result);
+        },
+        error: function (error) {
+            // console.log("error reaching API: " + error)
+        },
+    })
+}
